c48adc9d721729ef4bb3a62b29613c8570fdfd9a
omi-8055 <65586156+omi-8055@users.noreply.github.com>
2020-05-28 10:29:22 +0530

    Update 1422.py

--- 1422.py
+++ 1422.py
@@ -1,3 +1,33 @@
 def addition():
-  a=eval(input("enter value of first no:"))
-  
+  a=eval(input("enter value of first no:")
+  b=eval(input("Enter first number:"))
+    c=a+b
+    print("{}+{}={}".format(a,b,c))
+   
+def substraction():
+    a=eval(input("Enter first number:"))
+    b=eval(input("Enter first number:"))
+    c=a-b
+    print("{}-{}={}".format(a,b,c)) 
+ 
+def multiplication():
+    a=eval(input("Enter first number:"))
+    b=eval(input("Enter first number:"))
+    c=a*b
+    print("{}*{}={}".format(a,b,c))
+    
+def division():
+    a=eval(input("Enter first number:"))
+    b=eval(input("Enter first number:"))
+    c=a/b
+    print("{}/{}={}".format(a,b,c)) 
+print("Select option: \n 1 for addition\n 2 for substration \n 3 for multiplication \n 4 for division")
+option=int(input("Enter option given in above:"))
+if option==1:
+	addition()
+elif option==2:
+	substraction()
+elif option==3:
+	multiplication()
+elif option==4:
+	division()
